0fa61f9abe0dd29389c688332f7279713850efcb
Benjamin Arkorful <benprimeartist@gmail.com>
2025-11-05 15:50:45 +0000

    Completed the third assignment

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,7 @@
 const argument = process.argv.slice(2);
 
 if (argument.length === 0) {
-  console.log("No argument found")
+  console.log("No argument")
 }
 else if (argument.length === 1) {
   console.log("Argument found")
